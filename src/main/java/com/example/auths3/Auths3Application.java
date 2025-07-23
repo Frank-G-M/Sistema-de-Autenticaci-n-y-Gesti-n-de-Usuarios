70054b7a10f4e8ddf949f3568636c561e26caeb6
Frank-G-M <frankmayorgaguerrero@gmail.com>
2025-07-23 11:01:46 -0500

    initial commit

---
+++ src/main/java/com/example/auths3/Auths3Application.java
@@ -0,0 +1,13 @@
+package com.example.auths3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Auths3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Auths3Application.class, args);
+	}
+
+}
