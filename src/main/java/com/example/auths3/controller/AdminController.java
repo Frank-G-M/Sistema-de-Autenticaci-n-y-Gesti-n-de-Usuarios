27a720bf757978a265bff235a43f3f41d53a4a47
Frank-G-M <frankmayorgaguerrero@gmail.com>
2025-08-05 19:58:54 -0500

    Se implementa end-point para admin con permisos de SecurityConfig

---
+++ src/main/java/com/example/auths3/controller/AdminController.java
@@ -0,0 +1,37 @@
+package com.example.auths3.controller;
+
+
+import com.example.auths3.dto.UserDTO;
+import com.example.auths3.repository.RepositoryUser;
+import com.example.auths3.service.UserMapper;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/api/admin")
+public class AdminController {
+
+    private final RepositoryUser repositoryUser;
+
+    public AdminController (RepositoryUser repositoryUser){
+        this.repositoryUser = repositoryUser;
+    }
+
+    @GetMapping("/users")
+    @PreAuthorize("hasRole('ADMIN')")
+
+    public ResponseEntity<List<UserDTO>>getAllUsers(){
+        List<UserDTO> users = repositoryUser.findAll()
+                .stream()
+                .map(UserMapper::userDTO)
+                .collect(Collectors.toList());
+        return ResponseEntity.ok(users);
+    }
+
+}
